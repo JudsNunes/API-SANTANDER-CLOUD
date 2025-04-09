a264916e8da7cc616052e2b367d06156cbe49676
LUCAS N.N SILVA <95429639+JudsNunes@users.noreply.github.com>
2025-04-09 09:16:40 -0300

    Initial commit

---
+++ src/main/java/com/estudo/lucasdev/StudyLucasNunes2025Application.java
@@ -0,0 +1,15 @@
+ package com.estudo.lucasdev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyLucasNunes2025Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyLucasNunes2025Application.class, args);
+	}
+
+
+
+}
---
+++ src/test/java/com/estudo/lucasdev/StudyLucasNunes2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudo.lucasdev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyLucasNunes2025ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
